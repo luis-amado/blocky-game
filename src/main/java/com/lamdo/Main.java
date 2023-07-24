58d4433bc9d9b2be99bd139a48f27ba7447eb55e
Luis Amado <lamadoalvarez@gmail.com>
2023-07-24 10:39:20 -0600

    First Block

--- src/main/java/com/lamdo/Main.java
+++ src/main/java/com/lamdo/Main.java
@@ -23,23 +23,32 @@ public class Main {
 
         Window window = new Window();
 
-        float[] positions = {
-                -0.5f,  0.5f, -1.0f,
-                -0.5f, -0.5f, -1.0f,
-                 0.5f, -0.5f, -1.0f,
-                 0.5f,  0.5f, -1.0f
+        float[] positions = new float[]{
+                //north, south, east, west, up, down
+                1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0,
+                0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1,
+                1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0,
+                0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1,
+                0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+                1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0
         };
 
-        float[] textureCoords = {
-                0.0f, 0.0f,
-                0.0f, 1.0f,
-                1.0f, 1.0f,
-                1.0f, 0.0f
+        float[] textureCoords = new float[]{
+                0, 0, 0, 1, 1, 1, 1, 0,
+                0, 0, 0, 1, 1, 1, 1, 0,
+                0, 0, 0, 1, 1, 1, 1, 0,
+                0, 0, 0, 1, 1, 1, 1, 0,
+                0, 0, 0, 1, 1, 1, 1, 0,
+                0, 0, 0, 1, 1, 1, 1, 0
         };
 
-        int[] indices = {
-                0, 1, 2,
-                2, 3, 0
+        int[] indices = new int[]{
+                0, 1, 2, 2, 3, 0,
+                4, 5, 6, 6, 7, 4,
+                8, 9, 10, 10, 11, 8,
+                12, 13, 14, 14, 15, 12,
+                16, 17, 18, 18, 19, 16,
+                20, 21, 22, 22, 23, 20
         };
 
         RawModel model = Loader.loadToVAO(positions, textureCoords, indices);
